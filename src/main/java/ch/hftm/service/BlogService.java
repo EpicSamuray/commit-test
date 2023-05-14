18929adf05867e00f08eaab7a8a83bb1c00fabfa
Felice Pettinaro <felice.pettinaro@gmail.com>
2023-05-14 22:18:38 +0200

    feat(blog) added Simulation Repo / Model & Service - Testing file

---
+++ src/main/java/ch/hftm/service/BlogService.java
@@ -0,0 +1,29 @@
+package ch.hftm.service;
+
+import java.util.List;
+import org.jboss.logging.Logger;
+
+import ch.hftm.model.Blog;
+import ch.hftm.repository.BlogRepository;
+import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Inject;
+
+@Dependent
+public class BlogService {
+    @Inject
+    private BlogRepository blogRpy;
+
+    @Inject
+    private Logger logger;
+
+    public List<Blog> getBlogs() {
+        List<Blog> blogs = BlogRepository.getBlogs();
+        logger.info("Returning " + blogs.size() + " blogs");
+        return blogs;
+    }
+    
+    public void addBlog(Blog blog) {
+        logger.info("Adding blog " + blog.getTitle());
+        BlogRepository.addBlog(blog);
+    }
+}
