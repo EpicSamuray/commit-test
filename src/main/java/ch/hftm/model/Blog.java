883093e6af538c499c96bddf869964721124c64b
Felice Pettinaro <felice.pettinaro@gmail.com>
2023-05-15 23:19:45 +0200

    feat(panache) Added DB Connection and Entity and Some Test's

--- src/main/java/ch/hftm/model/Blog.java
+++ src/main/java/ch/hftm/model/Blog.java
@@ -1,16 +1,36 @@
 package ch.hftm.model;
 
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
-@AllArgsConstructor
-public class Blog {
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Blog implements Comparable<Blog>{
 
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NonNull
     private String title;
+    @NonNull
     private String description;
+
+    @Override
+    public int compareTo(Blog t) {
+        if(t.getId() == getId()) {
+            return 0;
+        }
+        return -1;
+    }
 
 }
